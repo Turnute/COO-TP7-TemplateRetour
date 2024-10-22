5cf0dbc7f0e7aa235594a238c8b54745579e54bb
Turnute <59569767+Turnute@users.noreply.github.com>
2024-10-22 11:21:49 +0200

    Rendu

--- TP7-Audebert-Tourenne/TP7-Audebert-Tourenne/TP7-Audebert-Tourenne/TP7-Audebert-Tourenne.cpp
+++ TP7-Audebert-Tourenne/TP7-Audebert-Tourenne/TP7-Audebert-Tourenne/TP7-Audebert-Tourenne.cpp
@@ -39,11 +39,11 @@ void testCoopointer()
     cout << "c4 count : " << c4.count() << ", c4 value : " << *c4 << endl;
     cout << "c5 count : " << c5.count() << ", c5 value : " << *c5 << endl << endl;
 
-	c2 = c3;                // Affectation de deux coopointers sur la même zone mémoire
-	c1 = c2;                // Affectation de deux coopointers provenants de zones mémoires différentes
+    c2 = c3;                // Affectation de deux coopointers sur la même zone mémoire
+    c1 = c2;                // Affectation de deux coopointers provenants de zones mémoires différentes
     c4 = c2;                // Affectation de deux coopointers provenants de zones mémoires différentes
 
-	// Affichage Console
+    // Affichage Console
     cout << "c1 count : " << c1.count() << ", c1 value : " << *c1 << endl;
     cout << "c2 count : " << c2.count() << ", c2 value : " << *c2 << endl;
     cout << "c3 count : " << c3.count() << ", c3 value : " << *c3 << endl;
@@ -52,8 +52,8 @@ void testCoopointer()
 
     c5 = c2;               // Affectation de deux coopointers provenants de zone mémoire différente
 
-	// Affichage Console
-	cout << "c5 count : " << c5.count() << ", c5 value : " << *c5 << endl;
+    // Affichage Console
+    cout << "c5 count : " << c5.count() << ", c5 value : " << *c5 << endl;
     //cout << "test2 : " << *test2 << endl << endl;       // Vérification de la destruction du pointeur dans la console, inutile avec valgrind
 }
 
@@ -62,26 +62,26 @@ void testCoounique()
     // Pointeurs alloués dynamiquement
     int* test = new int(2);
     int* test2 = new int(3);
-    
+
     // Déclaration de coopointers
     coo::coounique<int> c1(test);   // c1 = test
     coo::coounique<int> c2(test2);  // c2 = test2
 
-	// Affichage Console
+    // Affichage Console
     cout << "c1 unique : " << *c1 << endl;
     cout << "c2 unique : " << *c2 << endl << endl;
-        
+
     c1 = c2;                        // c2 devient nullptr, c1 devient 3.    
 
-	// Affichage Console
-	cout << "c1 unique apres transfert : " << *c1 << endl;
+    // Affichage Console
+    cout << "c1 unique apres transfert : " << *c1 << endl;
     cout << "c2 unique apres transfert : " << c2.isEmpty() << endl << endl;
 
     coo::coounique<int> c3(c1);     // c1 devient nullptr, c3 est initialisé à 3.
 
-	// Affichage Console
-	cout << "c1 unique apres transfert : " << c1.isEmpty() << endl;
-	cout << "c3 unique apres transfert : " << *c3 << endl;
+    // Affichage Console
+    cout << "c1 unique apres transfert : " << c1.isEmpty() << endl;
+    cout << "c3 unique apres transfert : " << *c3 << endl;
 
 
     // coo::coounique<int> c3;      // Interdit
---
+++ TP7-Audebert-Tourenne/TP7-Audebert-Tourenne/TP7-Audebert-Tourenne/coopointer.h
@@ -0,0 +1,163 @@
+//  Tourenne    Audebert
+//  Vincent     Dorine
+
+#pragma once
+#include <iostream>
+
+namespace coo
+{
+	template <typename T>
+	class coopointer
+	{
+		// Définition de la classe Compteur
+		class coorefcounter
+		{
+			unsigned int cmpt;
+
+		public:
+			// Constructeurs ==================
+			coorefcounter() : cmpt(0) { }
+
+			coorefcounter(const int& i) : cmpt(i) { }
+
+			unsigned int get() const {
+				return cmpt;
+			}
+
+			// Opérateurs	==================
+			coorefcounter& operator++() {
+				++cmpt;
+				return *this;
+			}
+
+			coorefcounter& operator--() {
+				--cmpt;
+				return *this;
+			}
+
+			coorefcounter& operator=(const coorefcounter& other) {
+				this->cmpt = other->cmpt;
+				return *this;
+			}
+
+			bool operator==(const int& i) const {
+				return (this->cmpt == i);
+			}
+		};
+
+		coorefcounter* cmpt;
+		T* ptr;
+	public:
+		// Constructeurs & Destructeur ================
+		coopointer() : cmpt(new coorefcounter(1)), ptr(nullptr) {}
+
+		coopointer(T* t) : cmpt(new coorefcounter(1)), ptr(t) {}
+
+		coopointer(const coopointer<T>& other) {
+			++(*other.cmpt);
+			cmpt = other.cmpt;
+			ptr = other.ptr;
+		}
+	
+		~coopointer(){
+			--(*cmpt);
+
+			if (*cmpt == 0)
+			{
+				delete cmpt;
+				delete ptr;
+			}
+		}
+		// Méthodes ================
+		unsigned int count() const {
+			return (*cmpt).get();
+		}
+
+		// Operateurs ================
+		T& operator*() const {
+			return *ptr;
+		}
+
+		T* operator->() {
+			return ptr;
+		}
+
+		coopointer<T>& operator=(const coopointer<T>& other) {
+			if (ptr != other.ptr)
+			{
+				--(*cmpt);
+				// Cas spécial : l'élément qui prend 
+				// l'affectation était le dernier alloué à sa zone mémoire
+				if (*cmpt == 0)
+				{
+					delete cmpt;
+					delete ptr;
+				}
+				// Sinon, on décrémente simplement son compteur
+
+				// Affectation en surface
+				cmpt = other.cmpt;
+				ptr = other.ptr;
+				++(*cmpt);
+			}
+
+			return *this;
+		}
+
+	};
+
+	template <typename T>
+	class coounique
+	{
+		T* ptr;
+
+		// Méthodes à interdire : définies en private
+		coounique();
+
+	
+	public:
+		// Constructeur & Destructeur ================
+		coounique(T* t) : ptr(t) { }
+
+		coounique(coounique<T>& other)
+		{
+			ptr = other.ptr;
+			other.ptr = nullptr;
+		}
+
+		~coounique(){
+			delete ptr;
+		}
+
+		// Méthodes ================
+		std::string isEmpty()
+		{
+			if (ptr)
+				return "not empty";
+			else return "nullptr";
+		}
+
+		// Operateurs ================
+		T& operator*() const {
+			return *ptr;
+		}
+
+		T* operator->() {
+			return ptr;
+		}
+
+		coounique<T>& operator=(coounique<T>& other)
+		{
+			delete ptr;
+
+			ptr = other.ptr;
+			other.ptr = nullptr;
+
+			return *this;
+		}
+
+	};
+
+}
+
+
